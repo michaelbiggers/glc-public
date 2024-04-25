f8b9b7b01760713a2a177d618073de6dd7eba7f6
Michael <michaelbiggers137@gmail.com>
2024-04-24 22:55:15 -0400

    feat: basic services page

---
+++ src/pages/services.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const ServicesPage = () => {
+	return (
+		<main>
+			<Layout pageTitle="Services">
+                <p>Our Services</p>
+            </Layout>
+		</main>
+	)
+}
+export const Head = () => <title>What We Offer</title>
+export default ServicesPage
