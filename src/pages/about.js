9e1e8b0fa5ce3db6372d996722c4ef1fe08187e0
Michael <michaelbiggers137@gmail.com>
2024-04-24 22:54:34 -0400

    feat: basic About page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const AboutPage = () => {
+	return (
+		<main>
+			<Layout pageTitle="About"></Layout>
+		</main>
+	)
+}
+export const Head = () => <title>About Me</title>
+export default AboutPage
