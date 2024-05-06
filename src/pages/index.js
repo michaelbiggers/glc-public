74255c1abb583671be3f5eea9e5bcd0e66dfa382
Michael <michaelbiggers137@gmail.com>
2024-05-06 17:16:58 -0400

    feat: testimonial section

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,17 +10,23 @@ const IndexPage = () => {
 			<Layout>
 				<Hero />
 				<TeamBar />
-				<section class="container-fluid testimonial">
-					<div className="row">
-						<div className="col-xs-12 col-sm-6">
+				<section class="container-fluid testimonial p0">
+					<div className="row center-xs middle-xs bg--pattern">
+						<div className="col-xs-12 col-sm-6 p0">
 							<StaticImage
 								src="../images/bg.jpg"
-								layout="fullWidth"
+								transformOptions={{ fit: "cover", cropFocus: "attention" }}
+								layout= "fullWidth"
+								height= {600}
+								duotone={{
+									highlight: "#004400", // Specify the highlight color
+									shadow: "#440044", // Specify the shadow color
+								}}
 							/>
 						</div>
 						<div className="col-xs-12 col-sm-6">
 							<div className="testimonial__content">
-								<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+								<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
 								<h4>Laura, child tested for autism</h4>
 							</div>
 						</div>
